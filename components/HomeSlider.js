0ef483e1cbc5ee712b5edabae96734d005163e1c
Raul Smith <raul.n.smith@gmail.com>
2022-12-19 15:06:16 -0700

    About page added functionality to home slider component

--- components/HomeSlider.js
+++ components/HomeSlider.js
@@ -3,7 +3,7 @@ import { Carousel } from "react-bootstrap";
 const HomeSlider = ({ image, carouselId }) => {
     return (
         <div className="col-4">
-            <Carousel fade controls={false} indicators={false} interval={7500}>
+            <Carousel fade controls={false} indicators={false} interval={7500} pause={false}>
                 <Carousel.Item>
                     <img src="https://paddywackgifts.com/img/blank.png" className="d-block w-100" alt="Blank image" />
                 </Carousel.Item>
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,7 +1,37 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Transition from '../../components/Transition';
+
 const About = () => {
+    const [aboutPage, setAboutPage] = useState();
+
+    useEffect(() => {
+        async function fetchData() {
+            const abouts = await axios(
+                'https://backend.paddywackgifts.com/public/api/about'
+            );
+
+            setAboutPage(abouts.data.data[0]);
+        };
+
+        fetchData();
+    }, [])
+
     return (
-        <div>
-            About page
+        <div className="container-fluid">
+            {aboutPage && (
+                <Transition>
+                    <div className="row pt-2">
+                        <div className="col-7 offset-1">
+                            <h2 className="display-3">{aboutPage.title}</h2>
+                            <div dangerouslySetInnerHTML={{ __html: aboutPage.description }} />
+                        </div>
+                        <div className="col-3">
+                            <img src={aboutPage.image_url} width="80%" alt="About Paddy Wack Gifts" />
+                        </div>
+                    </div>
+                </Transition>
+            )}
         </div>
     )
 }
