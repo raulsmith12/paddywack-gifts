61bcf67743eb0689a5dbaf257d469699b34f2658
Raul Smith <raul.n.smith@gmail.com>
2022-12-22 02:31:01 -0700

    Gallery functionality

--- pages/gallery/[id].js
+++ pages/gallery/[id].js
@@ -1,9 +1,46 @@
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
 const Item = () => {
+    const router = useRouter();
+    const { id } = router.query;
+    const [image, setImage] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const galleryImage = await axios(
+                `https://backend.paddywackgifts.com/public/api/galleries/${id}`
+            );
+
+            console.log(galleryImage.data.data);
+            setImage(galleryImage.data.data);
+        }
+
+        fetchData();
+    }, []);
+
     return (
-        <div className="container">
+        <div className="container-fluid">
             <div className="row">
                 <div className="col">
-                    <h1>Gallery</h1>
+                    <h2 className="display-1">Gallery Item</h2>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-3">
+                                <Link href="/gallery">Back to Gallery</Link><br /><br />
+                                <p className="display-6">{image.description}</p>
+                            </div>
+                            <div className="col-9">
+                                <img src={image.image_url} width="100%" alt={image.description} />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -1,9 +1,45 @@
+import axios from "axios";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
 const Gallery = () => {
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const galleryImages = await axios(
+                'https://backend.paddywackgifts.com/public/api/galleries'
+            );
+
+            console.log(galleryImages.data.data);
+            setImages(galleryImages.data.data.reverse());
+        }
+
+        fetchData();
+    }, []);
+
     return (
-        <div className="container">
+        <div className="container-fluid">
             <div className="row">
                 <div className="col">
-                    <h1>Gallery</h1>
+                    <h2 className="display-1">Gallery</h2>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <div className="container">
+                        <div className="row justify-content-center">
+                            {images.map(i => (
+                                <div className="col-4 pb-2 text-center" key={i.id}>
+                                    <div className="card-img-placeholder">
+                                        <Link href={"/gallery/" + i.id}>
+                                            <img src={i.thumbnail_url} width="95%" alt={i.description} />
+                                        </Link>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
